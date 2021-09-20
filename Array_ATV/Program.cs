420efab1d562163127859fb4923e8ad88a2388d7
PedroOliveira0609 <90793896+PedroOliveira0609@users.noreply.github.com>
2021-09-20 15:33:05 -0300

    Estrutura de Repeticao

---
+++ Array_ATV/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArrayATV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numeros;
+            numeros = new string [10];
+            numeros[0] = "1";
+            numeros[1] = "2";
+            numeros[2] = "3";
+            numeros[3] = "4";
+            numeros[4] = "5";
+            numeros[5] = "6";
+            numeros[6] = "7";
+            numeros[7] = "8";
+            numeros[8] = "9";
+            numeros[9] = "10";
+
+            for (int i = 0; i < numeros.Length ; i++)
+            {
+                Console.WriteLine($" Os números são : {numeros[i]}");
+            }
+        }
+    }
+}
